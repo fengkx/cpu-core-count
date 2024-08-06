f865dd341889280131d8d0039da21f37231afedb
fengkx <liangkx8237@gmail.com>
2024-08-07 00:18:12 +0800

    chore: ci

--- tests/test.mjs
+++ tests/test.mjs
@@ -4,26 +4,24 @@ import assert from "node:assert";
 import { after, before, describe, it } from "node:test";
 import { fileURLToPath } from "node:url";
 
-const dockerImage = `cpu-core-count:${randomUUID().slice(0, 5)}`.trimEnd();
+const dockerImage = `fengkx/cpu-core-count:${randomUUID().slice(0, 5)}`.trimEnd();
 
 const projectDir = fileURLToPath(new URL("..", import.meta.url));
-before(() => {
-  spwan.sync(`docker build -t ${dockerImage} ${projectDir}`, { stdio: "inherit" });
-});
-
-after(() => {
-  spwan.sync(`docker image rm ${dockerImage}`.trim());
-});
-
-after(async () => {
-});
 
 describe("report correct cpu count", () => {
+  before(() => {
+    spwan.sync(`docker build -t ${dockerImage} ${projectDir}`, { stdio: "inherit" });
+  });
+
+  after(() => {
+    spwan.sync(`docker image rm ${dockerImage}`.trim());
+  });
+
   const binCompatiblePlatforms = ["node", "bun"];
   for (const platform of binCompatiblePlatforms) {
     it(`should work in ${platform}`, () => {
       const expected = 1.5;
-      const { stdout } = spwan.sync(`docker run -i --rm --cpus ${expected} cpu-core-count ${platform} dist/bin.js`, {
+      const { stdout } = spwan.sync(`docker run -i --rm --cpus ${expected} ${dockerImage} ${platform} dist/bin.js`, {
         stdio: "pipe",
       });
       assert.strictEqual(Number(stdout), expected);
@@ -32,7 +30,7 @@ describe("report correct cpu count", () => {
 
   it("should work in deno", () => {
     const expected = 1.5;
-    const { stdout } = spwan.sync(`docker run -i --rm --cpus ${expected} cpu-core-count deno run -A tests/deno.ts`, {
+    const { stdout } = spwan.sync(`docker run -i --rm --cpus ${expected} ${dockerImage} deno run -A tests/deno.ts`, {
       stdio: "pipe",
     });
     assert.strictEqual(Number(stdout), expected);
